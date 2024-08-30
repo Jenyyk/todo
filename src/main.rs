e0b15669c6bc8da7d2a07ebb2d2060ce779f2a67
Userjoke <123212079+Jenyyk@users.noreply.github.com>
2024-08-30 15:10:47 +0200

    implemented adding tasks

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,73 @@
-use std::fs::File;
-use std::io::{stdin, stdout, Write, self, BufRead};
-use std::collections::HashMap;
+use std::process::ExitCode;
+use std::fs::{File, OpenOptions};
+use std::io::{stdin, self, Write, BufRead};
+use std::collections::BTreeMap;
 
-fn main() {
+fn main() -> ExitCode {
     loop {
-        let file = get_persistent_storage();
-        let lines = match buffer_file(file) {
+        let file = get_persistent_storage("./todo_data.txt");
+        let lines = match buffer_file(&file) {
             Ok(file_contents) => file_contents,
             Err(error) => panic!("{}", error),
         };
 
-        let mut line_map: HashMap<u8, String> = HashMap::new();
+        let mut line_map: BTreeMap<u8, String> = BTreeMap::new();
         for (index, line) in lines.enumerate() {
             if let Ok(line_content) = line {
                 line_map.insert(index.try_into().unwrap(), line_content);
             }
         }
 
+        for (num, line) in &line_map {
+            println!("> {num} {line}")
+        }
+
         let mut input = String::new();
-        stdin().read_line(&mut input).expect("incorrect input");
+        store_user_inputs(&mut input);
+        if input.trim() == "exit" {
+            return ExitCode::SUCCESS;
+        }
+        let mut iter = input.trim().split_whitespace();
+        match iter.next() {
+            Some("add") => todo_add(iter.collect::<Vec<&str>>().join(" "), &mut line_map),
+            _ => todo!(),
+        }
+
     }
 }
 
-fn get_persistent_storage() -> File {
-    let file_result = File::open("./todoData.txt");
-    let file = match file_result {
-        Ok(file) => file,
-        Err(..) => match File::create("./todoData.txt") {
-            Ok(created_file) => created_file,
-            Err(error) => panic!("{}", error),
-        },
-    };
-    file
+fn get_persistent_storage(path: &str) -> File {
+    OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path)
+        .unwrap()
 }
 
-fn buffer_file(file_input: File) -> io::Result<io::Lines<io::BufReader<File>>> {
+fn buffer_file(file_input: &File) -> io::Result<io::Lines<io::BufReader<&File>>> {
     Ok(io::BufReader::new(file_input).lines())
+}
+
+fn store_user_inputs(input: &mut String) {
+    println!();
+    print!("> ");
+    io::stdout().flush().expect("Failed to flush stdout");
+    stdin().read_line(input).expect("incorrect input");
+    println!();
+}
+
+fn todo_add(to_add: String, lines: &mut BTreeMap<u8, String>) {
+    let mut result = String::new();
+    for (_num, line) in lines {
+        result.push_str(line);
+        result.push('\n');
+    }
+    result.push_str(&to_add);
+    result.push('\n');
+    let mut file = get_persistent_storage("./todo_data.txt");
+    if let Err(error) = file.write_all(result.as_bytes()) {
+        panic!("{}", error);
+    }
+    let _ = file.flush();
 }
